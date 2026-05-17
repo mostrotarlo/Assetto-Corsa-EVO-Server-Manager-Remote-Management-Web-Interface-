31846798e4344b752cdf1df94599b21b438610d0
mostrotarlo <ellequadrosnc@gmail.com>
2026-05-17 20:38:17 +0200

    Updated build script for PyInstaller

--- build_exe.bat
+++ build_exe.bat
@@ -5,7 +5,7 @@ python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python -m pip install pyinstaller
 
-pyinstaller ^
+python -m PyInstaller ^
   --noconfirm ^
   --clean ^
   --onefile ^
